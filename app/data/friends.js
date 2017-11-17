1448a18a0dbca376a47ab50efc78c7c1774e8239
Amy Slover <amykslover@gmail.com>
2017-11-16 22:17:36 -0800

    Added complete routing logic and survey question formatting

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+var friendsArray = [
+  {
+    personName: "Bill Murray",
+    personPhoto:"http://www.fillmurray.com/284/196",
+    personScores: [ 5, 1, 3, 1, 9, 6, 8, 1, 3, 3 ]
+  },
+  {
+    personName: "Bill Murray",
+    personPhoto:"http://www.fillmurray.com/300/200",
+    personScores: [ 5, 4, 6, 5, 5, 4, 6, 5, 4, 4 ]
+  }
+];
+
+
+module.exports = friendsArray;
---
+++ app/routes/apiRoutes.js
@@ -0,0 +1,43 @@
+var friendsArray = require("../data/friends");
+
+module.exports = function(app) {
+
+	app.post('/api/friends', function(request, response) {
+		//Set the current user's answers to variables for future use
+		currentUser = request.body;
+		currentUserScores = request.body.personScores;
+		//biggestDifference is the biggest possible difference from two people
+		//all other user differences will be compared against this one
+		//then compared against each other until the smallest is found
+		var biggestDifference = 3486784401;
+		var currentUserMatchName;
+		var currentUserMatchPhoto;
+
+		for (var j = 0; j < friendsArray.length; j++) {
+			//Calculate the differences between the current user and
+			//each subsequent user in the array. The match name and
+			//current difference will keep on getting replaced as a better
+			//match is found.
+			var userDifference = 0;
+
+			for (var i = 0; i < currentUserScores.length; i++) {
+				userDifference += Math.abs(parseInt(friendsArray[j].personScores[i]) - parseInt(currentUserScores[i]))
+			}
+				if(userDifference < biggestDifference) {
+					biggestDifference = userDifference;
+					currentUserMatchName = friendsArray[j].personName;
+					currentUserMatchPhoto = friendsArray[j].personPhoto;
+				}
+		}
+		//Add current user to the array of possible friend matches
+		console.log(currentUserMatchName);
+		console.log(currentUserMatchPhoto);
+		console.log(userDifference);
+		friendsArray.push(currentUser);
+		response.json(true);
+	});
+
+	app.get('/api/friends', function(request, response) {
+		response.json(friendsArray);
+	});
+}
